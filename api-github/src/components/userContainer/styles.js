588a9891c8f0698f1ef6d6f934cdfbcfadcb764f
Shiellyn Ferreira <brunashiellyn@gmail.com>
2022-01-09 20:59:22 -0300

    alterando estilo do userConatiner

--- api-github/src/components/userContainer/styles.js
+++ api-github/src/components/userContainer/styles.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const Section = styled.section`
     width: 400px;
-    height: 300px;
     background: rgba(255,255,255,.95);
     border-radius: 10px;
     border-bottom: 10px solid #FF7A00;
