278f7013f2973dd7442f592c0d3b3c0e57299a75
Shiellyn Ferreira <brunashiellyn@gmail.com>
2022-01-09 20:26:19 -0300

    alterando o css do container

--- api-github/src/components/userDetails/styles.js
+++ api-github/src/components/userDetails/styles.js
@@ -5,7 +5,7 @@ export const Container = styled.div`
     height: 100px;
     display: flex;
     align-items: center;
-    justify-content: center;
+    flex-direction: column;
 `;
 
 export const Name = styled.h1`
