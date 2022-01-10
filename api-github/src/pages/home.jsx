d46e6e83bd22f41e0b23e307b6a0db9b07c570e0
Shiellyn Ferreira <brunashiellyn@gmail.com>
2022-01-09 21:02:22 -0300

    adicionando componente UserNumbers na home

--- api-github/src/pages/home.jsx
+++ api-github/src/pages/home.jsx
@@ -3,6 +3,7 @@ import Header from '../components/header';
 import UserContainer from '../components/userContainer';
 import UserPicture from '../components/userPicture';
 import UserDetails from '../components/userDetails';
+import UserNumbers from '../components/userNumbers';
 
 export default function Home() {
     return (
@@ -11,6 +12,7 @@ export default function Home() {
             <UserContainer>
                 <UserPicture url="https://avatars.githubusercontent.com/u/84290343?v=4" alternativeText= "img"/>
                 <UserDetails name ="Shiellyn Ferreira" login = "@ShiellynFerr" bio = "Oi eu sou a Shiellyn"/>
+                <UserNumbers/>
             </UserContainer>
         </Container>
     );
